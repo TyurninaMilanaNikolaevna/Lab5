8788e2d12101b135a3939278e99f3ad0c0627eeb
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-17 16:36:31 +0300

    Scripted auto-commit on change (2021-12-17 16:36:31) by gitwatch.sh

--- src/main/java/lab5/HttpClient.java
+++ src/main/java/lab5/HttpClient.java
@@ -16,6 +16,7 @@ public class HttpClient {
             .mapAsync(3, (request) -> {
                 Long startTime = System.currentTimeMillis();
                 return Dsl.asyncHttpClient()
+                        .prepa
     })
             >
 }
