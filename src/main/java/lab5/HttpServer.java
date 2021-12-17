f1d3d001a728ab6b8a8c4b5953d53090e2170f99
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-17 13:13:58 +0300

    Scripted auto-commit on change (2021-12-17 13:13:58) by gitwatch.sh

--- src/main/java/lab5/HttpServer.java
+++ src/main/java/lab5/HttpServer.java
@@ -1,6 +1,7 @@
 package lab5;
 
 import akka.NotUsed;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
@@ -9,11 +10,21 @@ import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import org.asynchttpclient.AsyncHttpClient;
 
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
 public class HttpServer {
+
+    private static final String HOST = "localhost";
+    private static final int PORT = 8080;
+
+    private ActorRef routeActor;
+
+    private HttpServer(ActorRef routeActor) {
+        this.routeActor = routeActor;
+    }
 
     public static void main(String[] args) throws IOException {
         System.out.println("start!");
@@ -21,11 +32,13 @@ public class HttpServer {
 
         final Http http = Http.get(actorSystem);
         final ActorMaterializer actorMaterializer = ActorMaterializer.create(actorSystem);
+
+        final AsyncHttpClient asyncHttpClient = 
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
                 new src.main.java.AsyncHttpClient(actorSystem).flowHttp(actorMaterializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
-                ConnectHttp.toHost("localhost", 8080),
+                ConnectHttp.toHost(HOST, PORT),
                 actorMaterializer
         );
         System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
@@ -37,5 +50,9 @@ public class HttpServer {
         binding
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> actorSystem.terminate()); // and shutdown when done
+
+        try {
+
+        }
     }
 }
