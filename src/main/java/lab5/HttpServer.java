933b9a69a262f17372bcae9af14a82f412f12fd9
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-17 18:11:17 +0300

    Scripted auto-commit on change (2021-12-17 18:11:17) by gitwatch.sh

--- src/main/java/lab5/HttpServer.java
+++ src/main/java/lab5/HttpServer.java
@@ -6,21 +6,13 @@ import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
-import akka.http.javadsl.model.HttpEntities;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
-import akka.japi.Pair;
-import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
-import akka.stream.javadsl.Keep;
-import akka.stream.javadsl.Source;
 import org.asynchttpclient.AsyncHttpClient;
 
 import java.io.IOException;
-import java.time.Duration;
-import java.util.Collections;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
 import static org.asynchttpclient.Dsl.asyncHttpClient;
@@ -45,32 +37,8 @@ public class HttpServer {
 
         final AsyncHttpClient asyncHttpClient = asyncHttpClient();
 
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = Flow
-                .of(HttpRequest.class)
-                .map(
-                        request -> new Pair<>(
-                                request.getUri().query().getOrElse("testUrl", ""),
-                                Integer.parseInt(request.getUri().query().getOrElse("count", ""))
-                        )
-                ).mapAsync(3, (request) -> Patterns
-                        .ask(cacheActor, request, Duration.ofSeconds(5))
-                        .thenCompose((response) -> {
-                            if (response.getClass() == String.class) {
-                                return Source.from(Collections.singletonList(request))
-                                        .toMat(sink(), Keep.right()).run(actorMaterializer)
-                                        .thenApply((t) -> new Response(request.first(), t / request.second()));
-                            }
-                            else return CompletableFuture.completedFuture(response);
-                        })
-                ).map(param -> {
-                    cacheActor.tell(param, ActorRef.noSender());
-                    return HttpResponse.create()
-                            .withEntity(
-                                    HttpEntities.create(
-                                            ((Response) param).getResponseTime() + " " + ((Response) param).getHostName()
-                                    )
-                            );
-                });
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow
+                = new src.main.java.AsyncHttpClient(actorSystem).flowHttp(actorMaterializer);
 
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
