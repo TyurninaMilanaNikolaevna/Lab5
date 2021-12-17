9d2ff2b30daa95b07f684ec011f236f7da422edd
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-17 16:32:40 +0300

    Scripted auto-commit on change (2021-12-17 16:32:40) by gitwatch.sh

--- src/main/java/lab5/HttpClient.java
+++ src/main/java/lab5/HttpClient.java
@@ -4,11 +4,12 @@ import akka.actor.ActorSystem;
 import akka.stream.javadsl.Flow;
 import javafx.util.Pair;
 
+import java.util.Collection;
+
 public class HttpClient {
 
     private Sink<Pair<String, Integer>,
         return Flow.<Pair<String,Integer>>create()
-            .mapConcat((request) ->
-            )
+            .mapConcat((request) -> Collection)
             >
 }
