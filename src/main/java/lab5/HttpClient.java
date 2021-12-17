a89a70ff434e0c34cf9efb17ea84abfe94f4b43c
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-17 16:39:41 +0300

    Scripted auto-commit on change (2021-12-17 16:39:41) by gitwatch.sh

--- src/main/java/lab5/HttpClient.java
+++ src/main/java/lab5/HttpClient.java
@@ -20,7 +20,7 @@ public class HttpClient {
                         .prepareGet(request)
                         .execute()
                         .toCompletableFuture()
-                        .thenCompose((response -> CompletableFuture.completedFuture(System.currentTimeMillis())))
+                        .thenCompose((response -> CompletableFuture.completedFuture(System.currentTimeMillis() - startTime)))
     })
             >
 }
