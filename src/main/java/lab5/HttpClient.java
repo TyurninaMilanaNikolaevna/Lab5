c9130b805da235435533f787d6a7e554ea8737f5
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-17 16:40:20 +0300

    Scripted auto-commit on change (2021-12-17 16:40:20) by gitwatch.sh

--- src/main/java/lab5/HttpClient.java
+++ src/main/java/lab5/HttpClient.java
@@ -22,6 +22,6 @@ public class HttpClient {
                         .toCompletableFuture()
                         .thenCompose((response -> CompletableFuture.completedFuture(System.currentTimeMillis() - startTime)))
     })
-            .toMatS
+            .toMat(Sink.fold())
             >
 }
