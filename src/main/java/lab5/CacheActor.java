7acfc09297246307fe12dd577fa71b49379d64c9
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-17 13:30:41 +0300

    Scripted auto-commit on change (2021-12-17 13:30:41) by gitwatch.sh

--- src/main/java/lab5/CacheActor.java
+++ src/main/java/lab5/CacheActor.java
@@ -1,7 +1,9 @@
 package lab5;
 
 import akka.actor.AbstractActor;
+import akka.japi.Pair;
 
+import javax.xml.ws.Response;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,6 +14,12 @@ public class CacheActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .match()
+                .match(Pair.class, t-> {
+                    String key = (String) t.first();
+                    if (cache.containsKey(key))
+                        sender().tell(new Response(key, cache.get(key), self());
+                        else
+                            );
+                } )
     }
 }
