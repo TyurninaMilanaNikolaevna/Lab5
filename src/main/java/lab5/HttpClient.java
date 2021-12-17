e3500c3672364ca82b7880d520460e3599066b62
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-17 16:34:58 +0300

    Scripted auto-commit on change (2021-12-17 16:34:58) by gitwatch.sh

--- src/main/java/lab5/HttpClient.java
+++ src/main/java/lab5/HttpClient.java
@@ -12,6 +12,8 @@ public class HttpClient {
     private Sink<Pair<String, Integer>,
         return Flow.<Pair<String,Integer>>create()
             .mapConcat((request) -> Collections.nCopies(request.second(), request.first()))
-            .mapAsync(3, (request) -> )
+            .mapAsync(3, (request) -> {
+                Long startTime = 
+    })
             >
 }
