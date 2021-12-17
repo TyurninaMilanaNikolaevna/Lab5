2517cc9f51ed858876964fa7a38d4c02035668ef
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-17 12:59:41 +0300

    Scripted auto-commit on change (2021-12-17 12:59:41) by gitwatch.sh

--- src/main/java/lab5/HttpServer.java
+++ src/main/java/lab5/HttpServer.java
@@ -1,5 +1,28 @@
 package lab5;
 
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+
 public class HttpServer {
-    
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
+                ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов
+        метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress
+                RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate()); // and shutdown
+        when done
+    }
 }
